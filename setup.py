1eb6cede86d2f3e95167ee39447445b7c7542821
Naresh <naresh.regula@surgsci.uu.se>
2018-02-26 10:12:05 +0100

    pack

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = 'project',
+	version='1.0',
+	description='Package Gameof life',
+	url = 'https://github.com/NareshRegula/project',
+	author = 'Adapted by Naresh',
+	author_email= 'naresh.regula@surgsci.uu.se',
+	license= 'BSD',
+	py_modules=['GAOFLI'],
+	packages = [])
